f3d3b76ddbdcce6641aba4b424aa298f4a39b5b0
KuzminKirill <wee3dood@gmail.com>
2017-03-03 18:19:26 +0300

    deleting orders complete

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^logout_user/$', views.logout_user, name='logout_user'),
     url(r'^create_order/$', views.create_order, name='create_order'),
     url(r'^(?P<order_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<order_id>[0-9]+)/delete_order/$', views.delete_order, name='delete_order'),
 ]
